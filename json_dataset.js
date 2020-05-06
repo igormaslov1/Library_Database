9271776c09a44d1c6b3deada2fc9fd628322eb3a
Igor Maslov <60472105+igormaslov1@users.noreply.github.com>
2020-05-06 11:04:02 -0400

    Update json_dataset.js

--- json_dataset.js
+++ json_dataset.js
@@ -2,5 +2,12 @@ const library_data = {
     results:
         require('./package.json')
 }
+const library_data1 = {
+    results:
+        require('./tsconfig.json')
+}
 
 module.exports.library_data = library_data;
+
+module.exports.library_data1 = library_data1;
+
