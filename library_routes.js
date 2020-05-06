15e0569c274908e5c1604658ec8363d25983ae1c
Igor Maslov <60472105+igormaslov1@users.noreply.github.com>
2020-05-06 11:06:05 -0400

    Update library_routes.js

--- library_routes.js
+++ library_routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 var port = process.env.PORT || 2000;
 var app = express()
+const fs = require('fs')
 
 app.use(function (req,res,next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,19 +14,71 @@ app.listen(port,() => {
     console.log(`Server running at port `+port);
 });
 
-
 // Require the module that defines the JSON object.
 const dataset = require('./json_dataset');
 
+
+
 // extract the specific JSON object that stores a user's data
 const library_data = dataset.library_data;
+const library_data1 = dataset.library_data1;
+
 
 // Route return user with a specific id.
 // Notice how the filter HOV is used to retrieve the matching user.
 
+
+app.get('/library/addBook/:title/:titleID/:author/:authorID/:publisher/:publisherID/:genre/:genreID/:language/:isbn/:size/:year', function(req,res) {
+    var title = req.params['title'];
+    var titleID = req.params['titleID'];
+    var author = req.params['author'];
+    var authorID = req.params['authorID'];
+    var publisher = req.params['publisher'];
+    var publisherID = req.params['publisherID'];
+    var genre = req.params['genre'];
+    var genreID = req.params['genreID'];
+    var language = req.params['language'];
+    var isbn = req.params['isbn'];
+    var size = req.params['size'];
+    var year = req.params['year'];
+
+    let current_library = library_data1["results"];
+    var output_books = []
+    var i = 0;
+    for (let val of current_library) {
+        output_books[i] = val;
+        i++
+    }
+
+    const book = {
+        "title": title,
+        "titleID":titleID,
+        "author":author,
+        "authorID":authorID,
+        "publisher":publisher,
+        "publisherID:":publisherID,
+        "genre":genre,
+        "genreID":genreID,
+        "language":language,
+        "isbn":isbn,
+        "size":size,
+        "year":year
+    }
+    output_books[i] = book;
+    const jsonString = JSON.stringify(output_books, null, 2)
+
+    fs.writeFile('./tsconfig.json', jsonString, err => {
+        if (err) {
+            res.send('Error writing file', err)
+        } else {
+            res.send('Successfully wrote file')
+        }
+    })
+})
+
 app.get('/library/byauthor/:author', function(req,res) {
     var author_name = req.params['author'];
-    let current_library = library_data["results"]["Books"];
+    let current_library = library_data1["results"];
     var output_books = []
     var i = 0;
     for(let val of current_library) {
@@ -38,7 +91,7 @@ app.get('/library/byauthor/:author', function(req,res) {
 })
 
 app.get('/library/view_collection', function(req,res) {
-    let current_library = library_data["results"]["Books"];
+    let current_library = library_data1["results"];
     var output_books = []
     var i = 0;
     for (let val of current_library) {
@@ -50,7 +103,7 @@ app.get('/library/view_collection', function(req,res) {
 
 app.get('/library/byBookTitle/:title', function(req,res) {
     var title = req.params['title'];
-    let current_library = library_data["results"]["Books"];
+    let current_library = library_data1["results"];
     var output_books = []
     var i = 0;
     for(let val of current_library) {
@@ -64,7 +117,7 @@ app.get('/library/byBookTitle/:title', function(req,res) {
 
 app.get('/library/byGenre/:genre', function(req,res) {
     var genre = req.params['genre'];
-    let current_library = library_data["results"]["Books"];
+    let current_library = library_data1["results"];
     var output_books = []
     var i = 0;
     for(let val of current_library) {
@@ -78,7 +131,7 @@ app.get('/library/byGenre/:genre', function(req,res) {
 
 app.get('/library/byPublisher/:publisher', function(req,res) {
     var publisher = req.params['publisher'];
-    let current_library = library_data["results"]["Books"];
+    let current_library = library_data1["results"];
     var output_books = []
     var i = 0;
     for(let val of current_library) {
